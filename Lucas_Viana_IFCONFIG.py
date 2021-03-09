fe0d1f9e2d38d0ded6f4eb0680713a80f0ad6412
Lucas Silva Viana <80357765+lucasviana13@users.noreply.github.com>
2021-03-09 18:57:59 -0300

    Add files via upload

---
+++ Lucas_Viana_IFCONFIG.py
@@ -0,0 +1,26 @@
+# -*- coding: cp1252 -*-
+
+_author_='Lucas Silva Viana'
+###Pyhon para alterar o ip e mascara em Linux
+
+import subprocess
+import math ###Biblioteca para se usar Log
+
+def mascara(ips):
+	### codigo para definir a mascara de acordo
+	### com o numero de ips desejados
+	masc =32-(int(math.log(ips, 2))+1)
+	return masc
+
+
+interface= raw_input ("Placa de Rede: ") ###Pega a Placa de Rede
+ip= raw_input ("Novo IP: ") ###Pega o ip novo para a máquina
+comand = 'ifconfig ' ###Comando para alterar ip e mascara
+ips= input ("Números de máquinas você deseja ligar: ") ###Para determinar a mascara
+masc = mascara(ips) 
+cmd = comand + interface+' '+ ip + '/' + str(masc) ###Comando escrito
+###print(cmd)
+subprocess.call(cmd, shell=True) ###Execução
+subprocess.call ('ifconfig', shell=True) ###Impressão das novas configurações
+
+
